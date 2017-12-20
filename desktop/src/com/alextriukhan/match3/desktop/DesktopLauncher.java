73f1bde888cec971cf33385e80d19e55046a8ed2
PatriotCodes <patriot130690@gmail.com>
2017-12-20 03:24:57 +0200

    Libgdx project added

---
+++ desktop/src/com/alextriukhan/match3/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alextriukhan.match3.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alextriukhan.match3.DiamondStoryGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DiamondStoryGame(), config);
+	}
+}
---
+++ ios/src/com/alextriukhan/match3/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.alextriukhan.match3;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.alextriukhan.match3.DiamondStoryGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new DiamondStoryGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
