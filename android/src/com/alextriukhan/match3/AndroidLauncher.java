08a252598df68c5589c609bfd8ac57fa8b26a313
PatriotCodes <patriot130690@gmail.com>
2018-08-02 13:21:49 +0300

    Fix for android build

---
+++ android/src/com/alextriukhan/match3/AndroidLauncher.java
@@ -0,0 +1,14 @@
+package com.alextriukhan.match3;
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.numSamples = 2;
+        initialize(DiamondStoryGame.INSTANCE, config);
+    }
+}
